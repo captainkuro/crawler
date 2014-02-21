6b8d030576a9724aa7e0d94ef3844209d95e97cb
Khandar William <khandar.william@gdpventure.com>
2014-02-21 14:23:24 +0700

    res

--- extractors/Scnsrc_Extractor.php
+++ extractors/Scnsrc_Extractor.php
@@ -1,9 +1,9 @@
 <?php
 
-class Rlsbb_Extractor implements Extractor {
+class Scnsrc_Extractor implements Extractor {
 	
 	public function can_extract($url) {
-		return strpos($url, 'http://www.rlsbb.com') === 0;
+		return strpos($url, 'http://www.scnsrc.me') === 0;
 	}
 
 	public function extract($columns, $s, $n, $url) {
@@ -19,39 +19,31 @@ class Rlsbb_Extractor implements Extractor {
 				$item = array();
 
 				if (in_array('link', $columns)) {
-					$title_a = $post->find('.postTitle', 0)->find('a', 0);
+					$title_a = $post->find('h2', 0)->find('a', 0);
 					$item['link'] = "<a href='{$title_a->href}'>link</a>";
 				}
 				if (in_array('title', $columns)) {
-					$title_a = $post->find('.postTitle', 0)->find('a', 0);
+					$title_a = $post->find('h2', 0)->find('a', 0);
 					$item['title'] = $title_a->innertext;
 				}
 				if (in_array('date', $columns)) {
-					$subtitle = $post->find('.postSubTitle', 0);
-					$date = Text::create($subtitle->innertext)->regex_match('/Posted on (.*) in </');
-					$date = $date[1];
-					$item['date'] = $date;
+					$date = $post->find('span.date', 0);
+					$item['date'] = $date->innertext;
 				}
 				if (in_array('category', $columns)) {
-					$subtitle = $post->find('.postSubTitle', 0);
+					$meta = $post->find('div.meta', 0);
 					$categories = array();
-					foreach ($subtitle->find('a[rel=category tag]') as $c) {
+					foreach ($meta->find('a[rel=category tag]') as $c) {
 						$categories[] = $c->innertext;
 					}
 					$item['categories'] = implode(', ', $categories);
 				}
 				if (in_array('content', $columns)) {
-					$content = $post->find('.postContent', 0);
-					if (!$content) {
-						$content = $post->find('.entry-content', 0);
-					}
+					$content = $post->find('.storycontent', 0);
 					$item['content'] = strip_tags($content->innertext, '<br>');
 				}
 				if (in_array('image', $columns)) {
-					$content = $post->find('.postContent', 0);
-					if (!$content) {
-						$content = $post->find('.entry-content', 0);
-					}
+					$content = $post->find('.storycontent', 0);
 					$img = $content->find('img', 0);
 					$item['image'] = $img ? $img->outertext() : '';
 				}
