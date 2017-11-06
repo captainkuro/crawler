ed374793762fe410f462be44c0598db73bf725d0
Captain kurO <kuro.senchou@gmail.com>
2017-11-07 00:31:44 +0700

    asldkjflksjfd

--- mangas/Mangastream_Crawler.php
+++ mangas/Mangastream_Crawler.php
@@ -5,6 +5,7 @@ class Mangastream_Crawler implements Manga_Crawler {
 
 	public function is_supported($url) {
 		return strpos($url, 'http://mangastream.com/') !== false
+			|| strpos($url, 'https://mangastream.com/') !== false
 			|| strpos($url, 'http://readms.com/') !== false
 			|| strpos($url, 'http://readms.net/') !== false;
 	}
